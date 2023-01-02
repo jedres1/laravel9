6e3e274687537f82492ea118210f772bcf2956b8
jedres1 <66815622+jedres1@users.noreply.github.com>
2023-01-01 23:21:53 -0600

    tutorial 14

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = DB::table('posts')->get();
+        $posts = Post::get();
         return view('blog',['posts' => $posts]);
+    }
+    public function show($post){
+        return Post::findOrFail($post);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','welcome')->name('home');
 Route::view('/contacto','contact')->name('contact');
+
 Route::get('/blog',[PostController::class,'index'])->name('blog');
+Route::get('/blog/{post}',[PostController::class,'show']);
 Route::view('/about','about')->name('about');
